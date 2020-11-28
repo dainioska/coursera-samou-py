52998a416ba6d6d3b4079b054b7f616d2f983953
dainioshka <dainioshka@gmail.com>
2020-11-28 09:08:54 +0200

    add for, game

---
+++ testas_for.py
@@ -0,0 +1,14 @@
+#####ciklai
+a = "example.net"
+if "my" in a or a.endswith(".net"):
+    print("OK")
+#####
+for i in range(5, 10, 2):
+    print(i)
+#####
+result = 0
+while True:
+    result += 1
+    if result >= 100:
+        break
+print(result)
