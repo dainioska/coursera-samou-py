e09e926ea95d6f1f4a1082faad78843ff1ef14b6
dainioska <dainioska@yahoo.com>
2020-12-04 13:55:32 +0200

    changes week03_02_ver.py

--- playenv/sprendimai/week03_02_ver01.py
+++ playenv/sprendimai/week03_02_ver01.py
@@ -1,11 +1,11 @@
 import csv
-import os.path
+import os
 
-class CarBase:
+class CarBase:   
     def __init__(self, brand, photo_file_name, carrying):
         self.brand = brand
         self.photo_file_name = photo_file_name
-        self.carrying = carrying
+        self.carrying = float(carrying)
 
     def set_brand(self, brand):
         self.brand = brand
@@ -14,28 +14,31 @@ class CarBase:
         self.photo_file_name = filename
 
     def set_carrying(self, carrying):
-        self.carrying = float(carrying)
+        self.carrying = carrying
 
     def get_photo_file_ext(self):
-        self.os.path.splitext(self.photo_file_name)[1]
+        return os.path.splitext(self.photo_file_name)[1]
+
 
 class Car(CarBase):
     def __init__(self, brand, photo_file_name, 
                  carrying, passenger_seats_count):
         super().__init__(brand, photo_file_name, carrying)
         self.passenger_seats_count = int(passenger_seats_count)
+        self.car_type = str('car')
 
     def set_passenger_seats_count(self, count):
-        self.passenger_seats_count = int(count)
+        self.passenger_seats_count = count
 
 
 class Truck(CarBase):
     def __init__(self, brand, photo_file_name, carrying, body_whl):
         super().__init__(brand, photo_file_name, carrying)
         #self.body_whl = body_whl
-        self.body_width = body_whl[:4]
-        self.body_height = body_whl[5:9]
-        self.body_length = body_whl[10:14]
+        self.body_width = float(body_whl[:4])
+        self.body_height = float(body_whl[5:9])
+        self.body_length = float(body_whl[10:14])
+        self.car_type = str('truck')
 
     def set_body_width(self, width):
         self.body_width = float(width)
@@ -53,18 +56,28 @@ class SpecMachine(CarBase):
     def __init__(self, brand, photo_file_name, carrying, extra):
         super().__init__(brand, photo_file_name, carrying)
         self.extra = extra
+        self.car_type = str('spec_machine')
 
     def set_extra(self, extra):
         self.extra = extra
 
 
-#########TESTAVIMAS
+#####TESTAVIMAS
 
 car = Car('Bugatti Veyron', 'bugatti.png', '0.312', '2')
-print(car.brand, car.photo_file_name, car.carrying, car.passenger_seats_count, sep='\n')
+print(car.car_type, car.brand, car.photo_file_name, car.carrying, car.passenger_seats_count, sep='\n')
 
 spec_machine = SpecMachine('Komatsu-D355', 'd355.jpg', '93', 'pipelayer specs')
-print( spec_machine.brand, spec_machine.carrying, spec_machine.photo_file_name, spec_machine.extra, sep='\n')
+print(spec_machine.car_type, spec_machine.brand, spec_machine.carrying, spec_machine.photo_file_name, spec_machine.extra, sep='\n')
 
 truck = Truck('Nissan', 'nissan.jpeg', '1.5', '3.92x2.09x1.87')
-print(truck.brand, truck.photo_file_name, truck.body_length,truck.body_width, truck.body_height, sep='\n')
+print(truck.car_type, truck.brand, truck.photo_file_name, truck.body_length,truck.body_width, truck.body_height, sep='\n')
+######
+base =CarBase('Bugatti Veyron', 'bugatti.png', '0.312')
+print(base.get_photo_file_ext())
+car =Car('Bugatti Veyron', 'bugatti.jpg', '0.312', '2')
+print(car.get_photo_file_ext())
+print(car.set_passenger_seats_count(5))
+truck = Truck('Nissan', 'nissan.jpeg', '1.5', '3.92x2.09x1.87')
+truck.set_body_length(10.22)
+print(truck.get_body_volume())
