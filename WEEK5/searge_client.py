9c9c969b7f05546985d225266a567a6f6c38f71a
dainioshka <dainioshka@gmail.com>
2020-12-15 13:56:12 +0200

    add client versions

---
+++ WEEK5/searge_client.py
@@ -0,0 +1,80 @@
+import socket
+import time
+
+
+class ClientError(Exception):
+    pass
+
+
+class ClientSocketError(ClientError):
+    """Исключение, выбрасываемое клиентом при сетевой ошибке"""
+    pass
+
+
+class ClientProtocolError(ClientError):
+    """Исключение, выбрасываемое клиентом при ошибке протокола"""
+    pass
+
+
+class Client:
+    """docstring for Client"""
+
+    def __init__(self, host, port, timeout=None):
+        self._host = host
+        self._port = int(port)
+        self._timeout = int(timeout)
+
+    def _send(self, cmd):
+        with socket.create_connection((self._host, self._port),
+                                      self._timeout) as sock:
+            sock.sendall(cmd.encode("utf8"))
+            data = sock.recv(1024)
+            return data.decode('utf-8')
+
+    def put(self, key, value, timestamp=None):
+        """Зберігаємо метрики на сервері"""
+        timestamp = timestamp or int(time.time())
+        try:
+            response = self._send(f"put {key} {value} {timestamp}\n")
+            if response[0:3] != 'ok\n':
+                raise ClientError(response)
+        except socket.error as err:
+            raise ClientError("Can't send data", err)
+
+    def get(self, key):
+        """отримуємо метрики"""
+        response = self._send(f"get {key}\n")
+        if response[0:3] != 'ok\n':
+            raise ClientError(response)
+
+        data = dict()
+        lines = response.split('\n')
+        for l in lines[1:-2]:
+            metric = l.split(' ')
+            res_key = metric[0]
+            res_val = float(metric[1])
+            res_ts = int(metric[2])
+            if res_key not in data:
+                data[res_key] = list()
+            data[res_key].append((res_ts, res_val))
+            data[res_key].sort(key=lambda tup: tup[0])
+
+        return data
+
+
+def _main():
+    client = Client("127.0.0.1", 8888, timeout=15)
+
+    # client.put("palm.cpu", 0.5, timestamp=1150864247)
+    # client.put("palm.cpu", 2.0, timestamp=1150864248)
+    # client.put("palm.cpu", 0.5, timestamp=1150864248)
+
+    # client.put("eardrum.cpu", 3, timestamp=1150864250)
+    # client.put("eardrum.cpu", 4, timestamp=1150864251)
+    # client.put("eardrum.memory", 4200000)
+
+    print(client.get("*"))
+
+
+if __name__ == '__main__':
+    _main()
--- WEEK5/week5_01.py
+++ WEEK5/week5_01.py
@@ -1,44 +1,28 @@
+#CLIENT
 import socket
 import time
 
-ClientError = 'klaida'
+class ClientError(Exception):
+    pass
 
 class Client:
     def __init__(self, host, port, timeout=None):
         self.host = host
         self.port = port
-        self.timeout = timeout        
-        self.sock = socket.create_connection((self.host, self.port), self.timeout)
-        
+        self.timeout = timeout  
 
-    def get(self, key):   
-        metr_dict = {}
-        send_data = f'get {key}\n'.encode('utf8')
-        self.sock.sendall(send_data)   
-        response = self.sock.recv(1024)
-        if( b'ok\n' not in response):
-                print("klaida")
+    def _send(self, cmd):      
+        with socket.create_connection((self.host, self.port), self.timeout) as sock:
+            sock.sendall(cmd.encode("utf8"))
+            data = sock.recv(1024)
+            return data.decode('utf-8')
 
-        response = str(response).strip('\n').split('\n')
 
-        for m in response:
-            metr = m.split(' ')
-            if len(metr) == 3:
-                metr_key = metr[0]
-                metr_value = float(metr[1])
-                metr_timestamp =int(metr[2])
-                metr_list = metr_dict.get(metr_key,[])
-                metr_list.append(metr_timestamp, metr_value)
-                metr_dict.update({metr_key: sorted(metr_list)})
-            #elif metr not in [[""], ["'"]]:
-                #print('blogai')
-
-            
-        return metr_dict
-
-########
-
+######
 if __name__ == '__main__':
      client = Client('127.0.0.1', 8888, timeout=15)
      print("OK")
+     #client.put("palm.cpu", 5.5, timestamp=1150864247)
+     #client.put("eardrum.cpu", 4, timestamp=1150864251)
+     #client.put("eardrum.memory", 4200000)
      print(client.get("*"))
