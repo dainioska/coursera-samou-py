a10307201d94bc87c5d19a2f5f7f5ea075fb3070
dainioshka <dainioshka@gmail.com>
2020-12-17 15:45:58 +0200

    korekcijos

--- playground/mymodule.py
+++
@@ -1,7 +0,0 @@
-from  mypack.utils import multiply
-
-if __name__ == "__main__":
-    print(multiply(2, 3))
-
-    
-            
--- teorija/solution01_02.py
+++
@@ -1,6 +0,0 @@
-import sys
-import math
-
-a = int(sys.argv[1])
-
-#not finished..................
