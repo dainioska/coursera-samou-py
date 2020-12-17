b2427e474a16d93f0e81d378e31c80eb2cb4ec78
dainioshka <dainioshka@gmail.com>
2020-12-17 15:40:35 +0200

    add week1

---
+++ WEEK1/week01_02.py
@@ -0,0 +1,19 @@
+#example: >python3 solution.py 3
+import sys
+
+digit_string = sys.argv[1]
+sum = int(digit_string)
+
+
+for i in range(1, sum + 1):
+    for j in range(1, sum + 1):
+        if(j <= sum - i ):
+           print(" ", end = '')
+        else:
+           print("#", end = '')
+    print()
+print()
+    
+   
+
+
---
+++ WEEK1/week01_03.py
@@ -0,0 +1,14 @@
+#example: >python3 solution.py 13 236 -396
+import sys 
+
+a = int(sys.argv[1]) 
+b = int(sys.argv[2]) 
+c = int(sys.argv[3])
+
+d_d = b**2-(4*a*c)
+d = d_d**(1/2)
+sol1 = ((-b)+d) / (2*a)
+sol2 = ((-b)-d) / (2*a)
+
+print(int(sol1))
+print(int(sol2))
